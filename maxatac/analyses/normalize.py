issue #8 Normalization fix, roi cleanup, add ATAC-seq  + CHIP scripts.

maxatac/analyses/normalize.py
@@ -3,11 +3,11 @@ import pyBigWig
 import os
 import tqdm
 from maxatac.utilities.system_tools import get_dir, Mute
+import sys
 
 with Mute():
     from maxatac.utilities.genome_tools import build_chrom_sizes_dict
     from maxatac.utilities.normalization_tools import find_genomic_min_max, minmax_normalize_array
-    from maxatac.utilities.constants import DEFAULT_CHRS
     import numpy as np
 
 
@@ -30,6 +30,7 @@ def run_normalization(args):
 
     :return: A minmax normalized bigwig file
     """
+    # Set up the names and directories
     basename = os.path.basename(args.signal).split(".bw")[0]
 
     OUTPUT_FILENAME = os.path.join(args.output, basename + "_minmax01.bw")
@@ -42,35 +43,36 @@ def run_normalization(args):
                   "\n  Output directory: " + output_dir
                   )
 
-    chromosome_length_dictionary = build_chrom_sizes_dict(args.chromosomes, args.chrom_sizes)
+    # Build a dictionary of chrom sizes to use to write the bigwig
+    chromosome_length_dictionary = build_chrom_sizes_dict(args.chroms, args.chrom_sizes)
 
-    genome_min, genome_max = find_genomic_min_max(args.signal)
+    # Find the genomic min and maximum values
+    genome_min, genome_max = find_genomic_min_max(args.signal, chrom_sizes_dict=chromosome_length_dictionary)
 
     logging.error("Normalize and Write BigWig file")
 
     # TODO Parallelize this code for each chromosome
     with pyBigWig.open(args.signal) as input_bw, pyBigWig.open(OUTPUT_FILENAME, "w") as output_bw:
-        header = [(x, chromosome_length_dictionary[x]) for x in sorted(DEFAULT_CHRS)]
+        header = [(x, chromosome_length_dictionary[x]) for x in sorted(args.chroms)]
 
         output_bw.addHeader(header)
-
-        # Create a status bar for to look fancy and count what chromosome you are on
-        chrom_status_bar = tqdm.tqdm(total=len(DEFAULT_CHRS), desc='Chromosomes Processed', position=0)
 
         for chrom_name, chrom_length in header:
             chr_vals = np.nan_to_num(input_bw.values(chrom_name, 0, chrom_length, numpy=True))
 
             normalized_signal = minmax_normalize_array(chr_vals, genome_min, genome_max)
 
-            output_bw.addEntries(
-                chroms=chrom_name,
-                starts=0,
-                ends=chrom_length,
-                span=1,
-                step=1,
-                values=normalized_signal.tolist()
-            )
+            output_bw.addEntries(chroms=chrom_name,
+                                 starts=0,
+                                 ends=chrom_length,
+                                 span=1,
+                                 step=1,
+                                 values=normalized_signal.tolist()
+                                 )
 
-            chrom_status_bar.update(1)
+        logging.error("Results saved to: " + output_dir)
 
-    logging.error("Results saved to: " + output_dir)
+    input_bw.close()
+    output_bw.close()
+
+    sys.exit()

maxatac/utilities/normalization_tools.py
@@ -3,13 +3,13 @@ import os
 import numpy as np
 import pandas as pd
 import pyBigWig
-import tqdm
 
 
-def find_genomic_min_max(bigwig_path):
+def find_genomic_min_max(bigwig_path, chrom_sizes_dict):
     """
     Find the genomic minimum and maximum values in the chromosomes of interest
 
+    :param chrom_sizes_dict: (dict) A dictionary of chromosome sizes filtered for the chroms of interest
     :param bigwig_path: (str) Path to the input bigwig file
 
     :return: Genomic minimum and maximum values
@@ -19,15 +19,13 @@ def find_genomic_min_max(bigwig_path):
     with pyBigWig.open(bigwig_path) as input_bigwig:
         minmax_results = []
 
-        # Create a status bar to look fancy and count what chromosome you are on
-        chromosome_status_bar = tqdm.tqdm(total=len(input_bigwig.chroms()), desc='Chromosomes Processed', position=0)
+        if not chrom_sizes_dict:
+            chrom_sizes_dict = input_bigwig.chroms()
 
-        for chromosome in input_bigwig.chroms():
+        for chromosome in chrom_sizes_dict:
             chr_vals = np.nan_to_num(input_bigwig.values(chromosome, 0, input_bigwig.chroms(chromosome), numpy=True))
 
             minmax_results.append([chromosome, np.min(chr_vals), np.max(chr_vals)])
-
-            chromosome_status_bar.update(1)
 
         logging.error("Finding genome min and max values")
 