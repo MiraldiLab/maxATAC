c971c2e49c83b6d45705babe3af8085890f8a7b5
Cazares, Tareian <Tareian.Cazares@cchmc.org>
2021-04-20 15:28:03 -0400

    fix call peaks function
    The call peaks function ended up producing the wrong peaks for our models because it as subtracting a 1 from the start position. This has been fixed.

--- maxatac/utilities/peak_tools.py
+++ maxatac/utilities/peak_tools.py
@@ -13,7 +13,7 @@ def get_genomic_locations(array, threshold, chromosome, bin_size):
     BIN_list = []
 
     for prediction_bin in target_bin_idx_list:
-        start = (prediction_bin[0] - 1)*bin_size
+        start = prediction_bin[0]*bin_size
         BIN_list.append([chromosome, start, start+bin_size])
 
     return pd.DataFrame(BIN_list, columns=["chr", "start", "end"])
