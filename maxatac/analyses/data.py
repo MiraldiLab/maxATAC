53e8208ecc5197ba04104bb84907892c9e73c852
FaizRizvi <rizvifw@mail.uc.edu>
2022-07-22 20:24:29 -0400

    Update Paths for different genome builds
    Updated genome build paths in accordance to user specified build (hg38, hg19, mm10)

--- maxatac/analyses/data.py
+++ maxatac/analyses/data.py
@@ -10,7 +10,7 @@ def run_data(args):
         args (Namespace): Arguments list for the reference genome and the log level. 
     """  
     check_data_packages_installed()
-    
+
     ############### Parameters ###############
     # Path to the directory where the project data should be installed. Default: ~/opt
     base_dir = get_dir(args.output)
@@ -26,13 +26,6 @@ def run_data(args):
 
     # Command to clone the data repo
     clone = "git clone https://github.com/MiraldiLab/maxATAC_data.git"
-    wget_2bit = f"wget --no-check-certificate https://hgdownload.cse.ucsc.edu/goldenpath/{args.genome}/bigZips/{args.genome}.2bit"
-
-    ############### Body ###############
-    logging.error(f"Downloading data for: {args.genome} \n" +
-                  f"Data will be installed: {args.output} \n" +
-                  f"Temporarily downlading data to: {maxatac_repo_dir} \n" +
-                  f"Final data will be placed in: {maxatac_final_dir}")
 
     os.chdir(maxatac_dir) # Change to the directory where the repo should be cloned
     
@@ -40,7 +33,19 @@ def run_data(args):
 
     dest = shutil.move(maxatac_repo_dir, maxatac_final_dir) # Rename the git directory to data
 
-    os.chdir(os.path.join(maxatac_final_dir, args.genome)) # change to the final data directory
-    os.system(wget_2bit) # Wget hg38.2bit
+    # Set all genomes if selected
+    if args.genome == ['all']:
+        args.genome = ["hg38", "hg19", "mm10"]
+
+    for genome in args.genome:
+        os.chdir(os.path.join(maxatac_final_dir, genome)) # change to the final data directory
+        wget_2bit = f"wget --no-check-certificate https://hgdownload.cse.ucsc.edu/goldenpath/{genome}/bigZips/{genome}.2bit"
+
+        ############### Body ###############
+        logging.error(f"Downloading data for: {genome} \n" +
+                      f"Data will be installed: {args.output} \n" +
+                      f"Temporarily downlading data to: {maxatac_repo_dir} \n" +
+                      f"Final data will be placed in: {maxatac_final_dir}")
+        os.system(wget_2bit) # Wget 2bit
 
     logging.error("Finished!")
